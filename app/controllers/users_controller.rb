7e05537bb2681df0e2863949f30dd56b11ddcb63
Takaaki <your.email@example.com>
2020-08-29 14:32:03 +0000

    20200829

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
-
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+ 
+  
+  def index
+    @users = User.all
+  end
+  
+   def index
+    @users = User.paginate(page: params[:page])
+   end
+  
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -11,18 +23,54 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+       flash[:success] = "Profile updated"
+      redirect_to @user
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
   private
 
-    def user_params
+   def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+  
+    end
+    
+
+    # beforeフィルター
+
+    # 正しいユーザーかどうかを確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうかを確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
